be9c0a8f83ca10015eab8e984ce8b030fcd221c6
Jacob <jacob.scott3141@gmail.com>
2021-06-30 09:57:52 +0100

    added basic contract for creating an appointment date

--- contracts/src/main/kotlin/com/template/contracts/TemplateContract.kt
+++ contracts/src/main/kotlin/com/template/contracts/TemplateContract.kt
@@ -1,6 +1,6 @@
 package com.template.contracts
 
-import com.template.states.TemplateState
+import com.template.states.AvailableAppointmentDate
 import net.corda.core.contracts.CommandData
 import net.corda.core.contracts.Contract
 import net.corda.core.contracts.requireSingleCommand
@@ -20,11 +20,11 @@ class TemplateContract : Contract {
     override fun verify(tx: LedgerTransaction) {
         // Verification logic goes here.
         val command = tx.commands.requireSingleCommand<Commands.Create>()
-        val output = tx.outputsOfType<TemplateState>().first()
+        val output = tx.outputsOfType<AvailableAppointmentDate>().first()
         when (command.value) {
             is Commands.Create -> requireThat {
                 "No inputs should be consumed when sending the Hello-World message.".using(tx.inputStates.isEmpty())
-                "The message must be Hello-World".using(output.msg == "Hello-World")
+                //"The message must be Hello-World".using(output.msg == "Hello-World")
             }
         }
     }
